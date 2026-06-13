90ecccf294bd0bd815b0e58014d923ee67399c8f
gabrielep26 <pisceddagabriele220@gmail.com>
2026-06-13 15:41:12 +0200

    base add_event structure, minor fix

--- app/routers/events.py
+++ app/routers/events.py
@@ -7,5 +7,13 @@ router = APIRouter(prefix="/events")
 
 @router.get("/")
 def get_all_events(session: SessionDep):
+    """Endpoint to obtain all events"""
     events = session.exec(select(Event)).all()
     return events
+
+@router.post("/")
+def add_event(session: SessionDep, event: Event):
+    """Endpoint to add a new event"""
+    session.add(Event.model_validate(event))
+    session.commit()
+    return "Event successfully added"
