70477d063be8f1ee28fef55d3bc7ea070d7bb2ca
gianlug0 <gianlucagarau2005@hotmail.com>
2026-06-14 16:22:18 +0200

    Add API get/events/{id}/register first draft. Minor fixes.

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,5 +1,14 @@
+from fastapi import APIRouter, HTTPException, Path
+from sqlmodel import select, delete
+from app.data.db import SessionDep
+from app.models.user import User
+from typing import Annotated, Any, Sequence
+
+router = APIRouter(prefix="/users")
+
 @router.get("/")
 def get_all_users(session: SessionDep):
+    """Returns the list of all users"""
     users = session.exec(select(User)).all()
     return users
 
