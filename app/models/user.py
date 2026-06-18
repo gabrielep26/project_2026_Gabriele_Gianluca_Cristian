a758fc5084e2a9b917a1349318fdd557de988753
gabrielep26 <pisceddagabriele220@gmail.com>
2026-06-18 21:19:38 +0200

    fix user

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,6 @@
 from sqlmodel import SQLModel, Field
 
-class UserBase(SQLModel, table=True):
+class User(SQLModel, table=True):
     username: str = Field(primary_key=True)
     name: str
     email: str
-
-class UserCreate(UserBase):
-    pass
-
-# 3. What is saved in the database (with "id")
-class User(UserBase,  table=True):
-    id: int | None = Field(default=None, primary_key=True)
