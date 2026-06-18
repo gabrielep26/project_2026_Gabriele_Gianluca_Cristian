f83c20ef84160e833eda404653037a96a8cd44eb
gianlug0 <gianlucagarau2005@hotmail.com>
2026-06-18 20:30:42 +0200

    Minor fixes

--- app/models/user.py
+++ app/models/user.py
@@ -3,8 +3,7 @@ from sqlmodel import SQLModel, Field
 class UserBase(SQLModel, table=True):
     username: str = Field(primary_key=True)
     name: str
-    #unique parameter makes email a unique constraint alongside the primary key
-    email: str = Field(unique=True)
+    email: str
 
 class UserCreate(UserBase):
     pass
