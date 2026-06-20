aa8d746d8d1ca2a9ce5aa68504db22a23e8cb128
gianlug0 <gianlucagarau2005@hotmail.com>
2026-06-20 12:09:37 +0200

    fixes model errors

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,18 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel,Field
 
-class User(SQLModel, table=True):
-    username: str = Field(primary_key=True)
+# 1. Base fields shared by everyone
+class UserBase(SQLModel):
+    username: str
     name: str
     email: str
+
+
+# 2. What the client sends in the POST request
+class UserCreate(UserBase):
+    pass
+
+
+# 3. What is saved in the database
+class User(UserBase, table=True):
+    username: str = Field(primary_key=True)
+    pass
